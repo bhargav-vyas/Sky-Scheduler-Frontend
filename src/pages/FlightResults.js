35e0da4f4658d47c493b9c089726f01b8a191b5b
Bhargav <vyasb1503@gmail.com>
2025-03-25 23:51:59 +0530

    this is the api of the  flight display

--- src/pages/FlightResults.js
+++ src/pages/FlightResults.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../Component/Header';
 import Footer from '../Component/Footer';
 
 
 const FlightResults = () => {
+    useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:6080/flights/search?departureAirport=JFK&arrivalAirport=LHR",); // Example API
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        // setData(result);
+      } catch (err) {
+        // setError(err.message);
+      }
+    };
+
+    fetchData();
+  }, []);
     return (
         <div className="flight-results">
             <Header />
