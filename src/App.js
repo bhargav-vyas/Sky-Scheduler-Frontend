a5e1fb8de242288547b2f9a0f1b419234eb62442
Bhargav <vyasb1503@gmail.com>
2025-03-29 23:30:56 +0530

    ading the termsand conduction pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Fixed typo
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import FlightResults from './pages/FlightResults';
-import Paymentpage from './pages/PaymentPage';
+import PaymentPage from './pages/PaymentPage';
+
+import TermsOfService from './pages/TermsOfService';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+
+import Security from './pages/Security';
 
 function App() {
   return (
     <Router>
+   
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/flights" element={<FlightResults />} />
-        <Route path="/payment" element={<Paymentpage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/terms" element={<TermsOfService />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        {/* <Route path="/cookies" element={<CookiePolicy />} /> */}
+        <Route path="/security" element={<Security />} />
       </Routes>
+    
     </Router>
   );
 }
---
+++ src/pages/Security.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Security = () => {
+    return (
+        <div className="page-container">
+            <h2>Security</h2>
+            <p>We take security seriously. Learn more about our measures...</p>
+        </div>
+    );
+};
+
+export default Security;
